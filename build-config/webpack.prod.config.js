018e19488c8d714ca08569a9e2ee9489784868fb
Lacey Sanderson <laceyannesanderson@gmail.com>
2020-11-16 14:53:43 -0600

    Ensure generic root is used for backwards compatibility.

--- build-config/webpack.prod.config.js
+++ build-config/webpack.prod.config.js
@@ -14,7 +14,7 @@ module.exports = {
     plugins: [new webpack.DefinePlugin({
             'process.env': {
                 NODE_ENV: JSON.stringify('production'),
-                DATADIR_PATH: JSON.stringify('sites/all/libraries/haplotype-map/dist/')
+                DATADIR_PATH: JSON.stringify('')
             }
         }),
         new TerserPlugin({
